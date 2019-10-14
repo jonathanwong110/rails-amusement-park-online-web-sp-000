be32d4d61c13b8b75ec4518bc4f150dbd587e7d5
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-14 04:19:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    render 'welcome/home'
+  end
+  
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def require_login
+    if !logged_in?
+      redirect_to root_path
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'application#index'
+  
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :attractions
+  resources :users, only: [:new, :create, :show]
 end
